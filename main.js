fba31a96bcc5ae77ce9880f677b229ce86f861fc
0xf1c40f <lbrthc@gmail.com>
2025-11-17 16:38:41 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hollow.node
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  bypass sfl.gl and others!
+// @match        *://*.tutwuri.id/*
+// @match        *://sfl.gl/ready/go*
+// @match        *://app.khaddavi.net/*
+// @match        *://bahasteknologi.com/*
+// @require      https://raw.githubusercontent.com/0xf1c40f/hollow.node---sfl.gl-tutwuri.id-etc./refs/heads/main/src/user.main.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @connect      *
+// @noframes
+// ==/UserScript==
